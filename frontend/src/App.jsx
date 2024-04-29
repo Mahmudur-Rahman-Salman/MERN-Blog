5ed0a9bda5f486eb3d77f42a81150b6810a084ce
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2024-04-30 03:28:09 +0600

    hero section added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
-
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header></Header>
       <Outlet></Outlet>
     </>
   );
