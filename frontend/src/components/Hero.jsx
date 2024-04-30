bcf79ab967fd3b6f51cab112ff76af3fd4e22242
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2024-05-01 00:14:05 +0600

    hero section edited

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,38 +2,54 @@ const Hero = () => {
   return (
     <>
       <section className="mt-24 mx-auto max-w-screen-xl pb-4 px-4 items-center lg:flex md:px-8">
-        <div className="space-y-4 flex-1 sm:text-center lg:text-left">
+        <div className="space-y-4 flex flex-col justify-center mx-auto sm:text-center lg:text-left">
           <h1 className="text-gray-800 font-bold text-4xl xl:text-5xl">
             Optimize your website for
             <span className="text-indigo-600"> Search engine</span>
           </h1>
-          <p className="text-gray-500 max-w-xl leading-relaxed sm:mx-auto lg:ml-0">
+          <p className="text-gray-500 max-w-xl leading-relaxed sm:mx-auto">
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout. The point
             of using Lorem Ipsum
           </p>
-          <div>
+          <div className="text-center">
             <p className="text-gray-800 py-3">
-              Subscribe to our newsletter and well save your time
+              Search the blog 
             </p>
-            <form className="items-center space-y-3 sm:justify-center sm:space-x-3 sm:space-y-0 sm:flex lg:justify-start">
-              <input
-                type="text"
-                placeholder="Enter your email"
-                className="text-gray-500 border outline-none p-3 rounded-md w-full sm:w-72"
-              />
+            <form className="items-center space-y-3 sm:justify-center sm:space-x-3 sm:space-y-0 sm:flex lg:justify-center">
+              <div className="relative">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="w-full p-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600 sm:w-72"
+                />
+              </div>
               <button className="outline-none bg-gray-700 text-white text-center px-4 py-3 rounded-md shadow w-full ring-offset-2 ring-gray-700 focus:ring-2  sm:w-auto">
-                Subscribe
+                Search
               </button>
             </form>
           </div>
         </div>
-        <div className="flex-1 text-center mt-4 lg:mt-0 lg:ml-3">
+        {/* <div className="flex-1 text-center mt-4 lg:mt-0 lg:ml-3">
           <img
             src="https://i.postimg.cc/kgd4WhyS/container.png"
             className="w-full mx-auto sm:w-10/12  lg:w-full"
           />
-        </div>
+        </div> */}
       </section>
     </>
   );
